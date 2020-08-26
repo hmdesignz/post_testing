16c1a3a973f4ff2b2a0544cddc69c31973ed5128
handmakerzhm <handmakerzhm@gmail.com>
2020-08-26 01:41:21 -0700

    file changes

--- routes/userActions.js
+++ routes/userActions.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const bcrypt = require('bcryptjs')
 const fs = require('fs')
 const router = express.Router();
 
-let timestamp = Date.now();
+let timestamp = Date.now().toLocaleString();
 
 router.get('/user/register', (req,res)=> {
     res.render('register');
@@ -22,13 +23,22 @@ router.get('/user/dashboard', (req,res)=> {
    
 });
 
+
+
 router.post('/user/register', (req,res)=> {
+    //checking the req.body elements
     const body = (`USERNAME:${req.body.user_name}/EMAIL: ${req.body.email}/PASSWORD:${req.body.password}`);
     res.render('register');
-    let timestamp = Date.now();
-    console.log(`${body}`);
-   
-});
+    /*setting up the salt and hashing method.
+    Always hash a user password to avoid malicious 
+    attacks and so forth.*/
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+    console.log(`User password is now hashed :^^^^${hashedPassword}`)
+    console.log(`${body}:\n${timestamp}`);
+    
+
+    });
 
 router.post('/user/login', (req,res)=> {
     const body = (`USERNAME:${req.body.user_name}/EMAIL: ${req.body.email}/PASSWORD:${req.body.password}`);
